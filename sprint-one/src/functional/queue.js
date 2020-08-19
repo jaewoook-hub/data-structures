3a3880794068681ab59c3f26a77200bc7183b699
Muhammad Meigooni <mmeigooni@gmail.com>
2020-08-19 17:13:18 -0600

    Complete implementation of functional queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,26 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var stackSize = 0;
+  var shiftedIndex = 0;
+
   someInstance.enqueue = function(value) {
+    storage[stackSize + shiftedIndex] = value;
+    stackSize++;
   };
 
   someInstance.dequeue = function() {
+    if (stackSize > 0) {
+      stackSize--;
+    }
+    var shiftedValue = storage[shiftedIndex];
+    delete storage[shiftedIndex];
+    shiftedIndex++;
+    return shiftedValue;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
