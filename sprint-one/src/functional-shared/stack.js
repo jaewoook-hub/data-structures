2b2f9cdc792c303dd3c3ca355996eccdd6ddace4
Muhammad Meigooni <mmeigooni@gmail.com>
2020-08-19 17:34:43 -0600

    Creates pseudocode for functional shared stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create a storage object
+  // extend storage by Stack.methods
+
+  // set stackSize to zero
+
+  // return a storage object
 };
 
-var stackMethods = {};
+Stack.methods = {
+
+  push: function(val) {
+
+  },
+  // pop: function() {}
+  // size: function() {}
+};
+
+
 
 
